28de6f8a88dda93a3bdb34d845a0f2b8aff2a2ce
mdsouza176 <melroydsouza176@gmail.com>
2018-05-25 13:29:04 -0700

    Create Object.java

---
+++ Object.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+/**
+ * Created by merli on 5/25/2018.
+ */
+public class Object {
+    public boolean visible;
+    public int width;
+    public int height;
+    public int x;
+    public int y;
+    ArrayList<ArrayList<Character>> ImageStyle;
+
+
+}
